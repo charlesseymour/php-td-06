aeff7fc8e4c1656fad882bed434db8283077370e
Charles Seymour <charlesseymour@yahoo.com>
2019-09-03 14:41:02 -0500

    Added getter tests

--- tests/ListingBasicCompleteTest.php
+++ tests/ListingBasicCompleteTest.php
@@ -14,7 +14,7 @@ class ListingBasicCompleteTest extends TestCase
 			'id' => 1, 'title' => 'LaraFest 2019', 
 		    'website' => 'https://www.larafest.org',
 			'email' => 'larafest@example.com', 'twitter' => '@larafest',
-			'status' => 'upcoming', 'image' => 'https://www.flickr.com/1234'
+			'image' => 'https://www.flickr.com/1234'
 		];
 		$this->listing = new ListingBasic($data);
 	}
@@ -22,5 +22,19 @@ class ListingBasicCompleteTest extends TestCase
 	public function testNewListingOK()
 	{
 		$this->assertIsObject($this->listing);
+	}
+	
+	public function testNewListingGetStatus() 
+	{
+		$this->assertEquals($this->listing->getStatus(), 'basic');
+	}
+	
+	public function testNewListingGetters()
+	{
+		$this->assertEquals($this->listing->getId(), 1);
+		$this->assertEquals($this->listing->getTitle(), 'LaraFest 2019');
+		$this->assertEquals($this->listing->getWebsite(), 'https://www.larafest.org');
+		$this->assertEquals($this->listing->getEmail(), 'larafest@example.com');
+		$this->assertEquals($this->listing->getTwitter(),'larafest'); 
 	}
 }
